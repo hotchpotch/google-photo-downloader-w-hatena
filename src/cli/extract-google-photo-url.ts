9be03e239441a269d8a572c6d3e1c574b1db6c57
Yuichi Tateno <hotchpotch@gmail.com>
2020-07-30 17:27:21 +0900

    glob for windows

--- src/cli/extract-google-photo-url.ts
+++ src/cli/extract-google-photo-url.ts
@@ -1,13 +1,22 @@
 import { program } from "commander"
 import { readFileSync } from "fs"
 import { googlePhotoUrlRegexp } from "../libs/googlePhotoUrlRegexp"
+import { glob } from 'glob'
 
 program.parse(process.argv)
 
-for (const file of program.args) {
-  const source = readFileSync(file).toString()
-  const m = source.match(googlePhotoUrlRegexp)
-  if (m) {
-    console.log(m.join("\n"))
+for (const arg of program.args) {
+  let files = []
+  if (arg.indexOf('*') >= 0) {
+    files = glob.sync(arg)
+  } else {
+    files = [arg]
+  }
+  for (const file of files) {
+    const source = readFileSync(file).toString()
+    const m = source.match(googlePhotoUrlRegexp)
+    if (m) {
+      console.log(m.join("\n"))
+    }
   }
 }
